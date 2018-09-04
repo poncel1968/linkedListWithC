6a1517277bc4851194020ed21c4527cc8bb65402
alumno <alumno@LAB00PC00>
2018-09-04 20:27:31 -0300

    arrays con maximo y minimo

--- Arrays/main.c
+++ Arrays/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define CANTIDAD 10
+#define CANTIDAD 5
 
 
 int main()
@@ -8,6 +8,10 @@ int main()
     int listadoDeNotas[CANTIDAD];
     int numeroIngresado;
     int i;
+    int mayor;
+    int menor;
+    int acumuladoNota = 0;
+
 /*
     printf(" Tamaño : %d \n",sizeof(numeroIngresado));
     printf(" Tamaño : %d \n",sizeof(listadoDeNotas));
@@ -22,22 +26,55 @@ int main()
 
     */
 
-    for(i=0;i<10;i++)
+    for(i=0;i<CANTIDAD;i++)
     {
         printf("Ingrese nota : ");
         scanf("%d",&numeroIngresado);
-
         listadoDeNotas[i]=numeroIngresado;
-
     }
 
     for(i=0;i<CANTIDAD;i++)
     {
         printf("La Nota es : %d \n",listadoDeNotas[i]);
 
+    }
 
+    for (i=0;i<CANTIDAD;i++)
+    {
+        if (i==0 || listadoDeNotas[i]>mayor)
+        {
+            mayor=listadoDeNotas[i];
+        }
 
     }
+    printf("\nEl mayor es : %d \n",mayor);
+
+
+    for (i=0;i<CANTIDAD;i++)
+    {
+        if (i==0 || listadoDeNotas[i]<mayor)
+        {
+            menor=listadoDeNotas[i];
+        }
+    }
+    printf("\nEl mayor es : %d \n",menor);
+
+    for (i=0;i<CANTIDAD;i++)
+    {
+            acumuladoNota=listadoDeNotas[i] + acumuladoNota;
+    }
+    printf("\nEl promedio es : %d \n",acumuladoNota/CANTIDAD);
+
+
 
     return 0;
 }
+
+
+int retornarTotal(int array[],int cant )
+
+
+    for (i=0;i<cant;i++)
+    {
+            acumuladoNota=listadoDeNotas[i] + acumuladoNota;
+    }
