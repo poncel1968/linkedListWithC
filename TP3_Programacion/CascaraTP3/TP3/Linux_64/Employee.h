03954ce1e375374037ace5ee1c39f563a4ce7d61
alumno <alumno@LAB00PC00>
2018-10-25 19:34:35 -0300

    2er commit

--- TP3_Programacion/CascaraTP3/TP3/Linux_64/Employee.h
+++ TP3_Programacion/CascaraTP3/TP3/Linux_64/Employee.h
@@ -24,4 +24,22 @@ int employee_getHorasTrabajadas(Employee* this,int* horasTrabajadas);
 int employee_setSueldo(Employee* this,int sueldo);
 int employee_getSueldo(Employee* this,int* sueldo);
 
-#endif // employee_H_INCLUDED
+
+
+Employee* Employee_new();
+void Employee_delete();
+Employee* Employee_newConParametros(int id,char* nombre,int horasTrabajadas,int sueldo);
+
+int Employee_setId(Employee* this,int id);
+int Employee_getId(Employee* this,int* id);
+
+int Employee_setNombre(Employee* this,char* nombre);
+int Employee_getNombre(Employee* this,char* nombre);
+
+int Employee_setHorasTrabajadas(Employee* this,int horasTrabajadas);
+int Employee_getHorasTrabajadas(Employee* this,int* horasTrabajadas);
+
+int Employee_setSueldo(Employee* this,int sueldo);
+int Employee_getSueldo(Employee* this,int* sueldo);
+
+#endif // EMPLOYEE_H_INCLUDED
